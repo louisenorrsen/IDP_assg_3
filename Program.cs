bba757f9decb3bb26ef2036b7a460034db8d9538
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2023-02-24 10:16:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IDP_assg_3
+{
+    internal class Program
+    {
+        class Grundämne
+        {
+            public string namn = string.Empty, typ=string.Empty;
+            public int Z;
+            public double smältpunkt, kokpunkt;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
